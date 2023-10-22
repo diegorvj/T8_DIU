d298cc122bd0133d815048156117b207b152a6f3
benjaminzunino <56267038+benjaminzunino@users.noreply.github.com>
2023-10-22 19:25:43 -0300

    Historial Personal realizado

--- src/components/historial_personal.js
+++ src/components/historial_personal.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Sidebar from './Sidebar';
 import './Index.css';
 import { useNavigate } from 'react-router-dom';
+import ContenidoOcultable from './ContenidoOcultable';
 
 function Index() {
 
@@ -26,11 +27,13 @@ function Index() {
             
           </div>
         
-        <h2 className='h1'style={{ textAlign: 'left', marginLeft: '15%', marginTop: '0%' }}>agregar contenido dependiendo de vista</h2>
-        <p style={{ textAlign: 'left', marginLeft: '20%', marginTop: '0%' }}>contenido</p>
-        <p style={{ textAlign: 'left', marginLeft: '20%', marginTop: '0%' }}>contenido</p>
-        <p style={{ textAlign: 'left', marginLeft: '20%', marginTop: '0%' }}>contenido</p>
-        <p style={{ textAlign: 'left', marginLeft: '20%', marginTop: '0%' }}>contenido</p>
+        <h2 className='h1'style={{ textAlign: 'left', marginLeft: '15%', marginTop: '0%' }}>Historial Médico</h2>
+        <ContenidoOcultable titulo="Visita 14 de Enero 2022" motivo="Examenes de rutina" explicacion="Paciente agenda hora para exámen pos sospechas de enfermedad, se encuentran sospechas de enfermedad" doctor="Dr. Felipe Moreno"/>
+        <ContenidoOcultable titulo="Visita 19 de Enero 2022" motivo="Detección de Enferemedad" explicacion="Exámenes arrojan resultados negativos, se procede a realizar estudios clínicos" doctor="Dr. Felipe Moreno"/>
+        <ContenidoOcultable titulo="Visita 29 de Enero 2022" motivo="Asiganción de Medicamentos" explicacion="Tras análisis clínico se le asigna medicamento al paciente y se le otorgan instrucciones de cuidados a sus tutores" doctor="Dr. Felipe Moreno"/>
+        <ContenidoOcultable titulo="Visita 31 de Julio 2022" motivo="Control de rutina" explicacion="Se realiza control al paciente, no existen cambios en el tratamiento puesto que la evolución de la enfermedad es positiva, mantener medicamentos" doctor="Dr. Felipe Moreno"/>
+        <ContenidoOcultable titulo="Visita 5 de Marzo 2023" motivo="Control de rutina" explicacion="Se realiza control al paciente, no existen cambios en el tratamiento puesto que la evolución de la enfermedad es positiva, mantener medicamentos" doctor="Dr. Felipe Moreno"/>
+        
       </div>
     </div>
   );
