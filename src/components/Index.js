94d04c2e6a75b19a36757fb9c396538836aa2ba4
diegorvj <riveradiegoa@gmail.com>
2023-10-22 12:54:21 -0300

    prueba imagenes

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import './Index.css';
+import image1 from '../imagenes/image1.jpg'; // Ruta a la primera imagen
+import image2 from '../imagenes/image2.jpg'; // Ruta a la segunda imagen
 
 function Index() {
   return (
     <div className="index-container">
       <h2>Página de Index</h2>
-      {/* Contenido de la página "index.js" */}
+      <div className="image-row">
+        <img src={image1} alt="Imagen 1" className="image" />
+        <img src={image2} alt="Imagen 2" className="image" />
+      </div>
     </div>
   );
 }
