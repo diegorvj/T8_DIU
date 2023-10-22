568f9f2ce5f90f0821ba999cc7fcc9b360ce9640
diegorvj <riveradiegoa@gmail.com>
2023-10-22 19:55:35 -0300

    hist familiar

--- src/components/historial_familiar.js
+++ src/components/historial_familiar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Sidebar from './Sidebar';
 import './Index.css';
 import { useNavigate } from 'react-router-dom';
+import ContenidoOcultable from './ContenidoOcultable';
 
 function Index() {
 
@@ -26,13 +27,25 @@ function Index() {
             
           </div>
         
-        <h2 className='h1'style={{ textAlign: 'left', marginLeft: '15%', marginTop: '0%' }}>agregar contenido dependiendo de vista</h2>
-        <p style={{ textAlign: 'left', marginLeft: '20%', marginTop: '0%' }}>contenido</p>
-        <p style={{ textAlign: 'left', marginLeft: '20%', marginTop: '0%' }}>contenido</p>
-        <p style={{ textAlign: 'left', marginLeft: '20%', marginTop: '0%' }}>contenido</p>
-        <p style={{ textAlign: 'left', marginLeft: '20%', marginTop: '0%' }}>contenido</p>
+        <h2 className='h1'style={{ textAlign: 'left', marginLeft: '15%', marginTop: '0%' }}>Historial Médico</h2>
+        <ContenidoOcultable titulo="Visita 5 de Agosto 2008" motivo="Detección de Enferemedad" explicacion="Paciente agenda hora para exámen pos sospechas de enfermedad, no se encuentran hallazgos" doctor="Dr. Alexis Sanchez"/>
+        <ContenidoOcultable titulo="Visita 26 de Noviembre 2010" motivo="Control de rutina" explicacion="Se realiza control al paciente, se requieren cambios en el tratamiento puesto que la evolución de la enfermedad es negativa." doctor="Dr. Tony Soprano"/>
+        <ContenidoOcultable titulo="Visita 17 de Julio 2012" motivo="Asiganción de Medicamentos" explicacion="Tras análisis clínico se le asigna medicamento al paciente y se le otorgan instrucciones de cuidados a sus tutores" doctor="Dr. Lionel Messi"/>
+        <ContenidoOcultable titulo="Visita 12 de Mayo 2020" motivo="Examenes de rutina" explicacion="Exámenes arrojan resultados negativos, se procede a realizar estudios clínicos" doctor="Dr. Juninho Sosa"/>
+        
       </div>
+
+      <br></br>
+
+      <button
+          className="btn float-left"
+          style={{ width: '200px', backgroundColor:'#5A7684', color: '#fff', marginRight: '51%'}}
+          onClick={() => navigate('/grupo_familiar.js')}>
+          Volver
+        </button>
     </div>
+
+
   );
 }
 
