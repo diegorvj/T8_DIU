cd1e8ba4f97f1688b19393bfd31949243d771ae2
diegorvj <riveradiegoa@gmail.com>
2023-10-22 12:35:27 -0300

    css

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import './Login.css';
 
 function Login({ setLoggedIn }) {
   const [email, setEmail] = useState('');
