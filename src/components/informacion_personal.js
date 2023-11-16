e6152b2027530f6983b8e291bcbb7671efbebb9e
diegorvj <riveradiegoa@gmail.com>
2023-11-16 16:55:56 -0300

    Reestructuracion inf pers

--- src/components/informacion_personal.js
+++ src/components/informacion_personal.js
@@ -4,6 +4,7 @@ import './informacion_personal.css';
 import { useNavigate } from 'react-router-dom';
 import fotoPerfil from '../imagenes/FotoPerfil.jpg';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Card, Button } from 'react-bootstrap';
 
 function Index() {
 
@@ -11,47 +12,50 @@ function Index() {
   return (
     <Sidebar>
       {/* Contenido principal aquí */}
+      
+      <h2 className='h1'style={{ textAlign: 'left', marginLeft: '5%', marginTop: '0%' }}>Informacion personal</h2>
 
-      <div className='cuerpo'>
-        
-        
-        
-        <h2 className='h1'style={{ textAlign: 'left', marginLeft: '15%', marginTop: '0%' }}>Información Personal</h2>
-        <div className="profile">
-          <div className="profile" style={{marginRight:'39%'}}>
-            <div className="profile-card">
-              <img src={fotoPerfil} alt="Foto de perfil" className="profile-photo" />
-                <h3 className="profile-title">Hernan Hernandez Ibarra</h3>
-                <p><strong>Numero de identificacion (DNI):</strong> 12345678-9</p>
-                <p><strong>Fecha Nacimiento:</strong> 01/01/1980</p>
-                <p><strong>Genero:</strong> Hombre</p>
-                <p><strong>Direccion:</strong> Calle Falsa 123</p>
-                <p><strong>Alergias:</strong> Ninguna</p>
-                <p><strong>Intolerancias alimenticias:</strong> Gluten</p>
-                <p><strong>Medicación crónica:</strong> Ninguna</p>
-            </div>
+      <Card className="m-4" bg="dark" text="white">
+        <Card.Body>
+
+          <div className="" style={{ textAlign: 'left' , marginLeft: 70}}>
+            <img
+              src={fotoPerfil}
+              alt="Foto de perfil"
+              style={{ width: '150px', height: '150px', marginRight: '1000px' }}  // Ajusta el ancho, alto y margen según tus necesidades
+            />
+            <br></br><br></br>
+            <h3 className="profile-title">Hernan Hernandez Ibarra</h3>
+            <p><strong>Numero de identificacion (DNI):</strong> 12345678-9</p>
+            <p><strong>Fecha Nacimiento:</strong> 01/01/1980</p>
+            <p><strong>Genero:</strong> Hombre</p>
+            <p><strong>Direccion:</strong> Calle Falsa 123</p>
+            <p><strong>Alergias:</strong> Ninguna</p>
+            <p><strong>Intolerancias alimenticias:</strong> Gluten</p>
+            <p><strong>Medicación crónica:</strong> Ninguna</p>
           </div>
-        </div>
+        </Card.Body>
+        <Card.Footer>
+          <div className="d-flex justify-content-center">
+            <Button
+              variant="primary"
+              style={{ width: '200px', backgroundColor: '#5A7684', color: '#fff' }}
+              onClick={() => navigate('/modificar_personal.js')}
+            >
+              Modificar
+            </Button>
+            <Button
+              variant="primary"
+              style={{ width: '200px', marginLeft: '20px', backgroundColor: '#5A7684', color: '#fff' }}
+              onClick={() => navigate('/Index.js')}
+            >
+              Volver
+            </Button>
+          </div>
+        </Card.Footer>
+      </Card>
 
-      </div>
-
-      <div style={{ display: 'flex', alignItems: 'center' }}>
-
-          <button
-            className="btn float-left"
-            style={{ width: '200px', backgroundColor:'#5A7684', color: '#fff', marginLeft: '20%'}}
-            onClick={() => navigate('/modificar_personal.js')}>
-            Modificar 
-          </button>
-
-          <button
-            className="btn float-left"
-            style={{ width: '200px', backgroundColor:'#5A7684', color: '#fff', marginLeft:'20px'}}
-            onClick={() => navigate('/Index.js')}>
-            Volver
-          </button>
-
-      </div>
+    
       
     </Sidebar>
     
