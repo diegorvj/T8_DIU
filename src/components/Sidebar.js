1f0b499c08209990a431e084dcbe5328ec1a5508
diegorvj <riveradiegoa@gmail.com>
2023-11-16 14:17:53 -0300

    Reestructuracion navbar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './Sidebar.css';
-import { useNavigate } from 'react-router-dom';
+import logo from '../imagenes/logo.png';
+import { Container, Navbar, Nav } from 'react-bootstrap';
+import { NavLink, useNavigate } from 'react-router-dom';
 
-function Sidebar() {
+function Sidebar( { children } ) {
   const navigate = useNavigate();
 
   const VolverALogin = () => {
@@ -13,59 +13,83 @@ function Sidebar() {
     window.location.href = 'http://localhost:3000/';
   };
   return (
-    <div>
-      <Navbar bg="dark" variant="dark" id="sidebar">
-        <Nav className="flex-column">
-          <Navbar.Brand>
-          <br></br>
-          <h2>Menu</h2>
-          </Navbar.Brand>
-          <br></br><br></br><br></br><br></br><br></br>
-          <Nav.Link className="nav-link">
-            <button
-              className="btn"
-              style={{ width: '200px', backgroundColor:'#5A7684', color: '#fff' }}
-              onClick={() => navigate('/informacion_personal.js')}>
-              Informacion Personal
-            </button>
-          </Nav.Link>
+    <Container fluid className="p-0">
 
-          <br></br>
-
-          <Nav.Link className="nav-link">
-            <button
-              className="btn"
-              style={{ width: '200px', backgroundColor:'#5A7684', color: '#fff' }}
-              onClick={() => navigate('/historial_personal.js')}>
-              Historial Medico
-            </button>
-          </Nav.Link>
-
-          <br></br>
-
-          <Nav.Link className="nav-link">
-            <button
-              className="btn"
-              style={{ width: '200px', backgroundColor:'#5A7684', color: '#fff' }}
-              onClick={() => navigate('/grupo_familiar.js')}>
-              Grupo Familiar
-            </button>
-          </Nav.Link>
-
-          <br></br>
-
-          <Nav.Link className="nav-link">
-            <button
-              className="btn"
-              style={{ width: '200px', backgroundColor:'#5A7684', color: '#fff' }}
-              onClick={VolverALogin}>
-              Cerrar Sesion
-            </button>
-          </Nav.Link>
-          
-        </Nav>
+      {/* Barra Superior */}
+      <Navbar bg="dark" variant="dark" className="py-5 d-flex align-items-center">
+        <div style={{ position: 'relative', width: '7%', marginLeft: '5%' }}>
+          <NavLink to="/Index.js">
+            <img src={logo} alt="Logo" className="img-fluid" style={{ width: '100%' }} />
+          </NavLink>
+        </div>
+        <Navbar.Brand className='mx-auto fs-1'>Clinica Generica</Navbar.Brand>
       </Navbar>
-    </div>
+
+      <Container fluid className="p-0 d-flex">
+        {/* Barra lateral vertical */}
+
+        <Nav className="col-md-2 d-none d-md-block bg-dark sidebar">
+          <div className="position-sticky" style={{ height: '100vh' }}>
+            <ul className="nav flex-column mt-4">
+              
+              <h2 className="text-white fs-1 text-center">
+                Menu
+              </h2>
+              <br></br><br></br><br></br>
+
+              <li className="nav-item">
+                <Nav.Link className="nav-link">
+                  <button
+                    className="btn w-75"
+                    style={{ backgroundColor: '#5A7684', color: '#fff'}}
+                    onClick={() => navigate('/informacion_personal.js')}>
+                    Informacion Personal
+                  </button>
+                </Nav.Link>
+              </li>
+
+              <li className="nav-item">
+                <Nav.Link className="nav-link">
+                  <button
+                    className="btn w-75"
+                    style={{ backgroundColor: '#5A7684', color: '#fff' }}
+                    onClick={() => navigate('/historial_personal.js')}>
+                    Historial Medico
+                  </button>
+                </Nav.Link>
+              </li>
+
+              <li className="nav-item">
+                <Nav.Link className="nav-link">
+                  <button
+                    className="btn w-75"
+                    style={{ backgroundColor: '#5A7684', color: '#fff', padding: 10 }}
+                    onClick={() => navigate('/grupo_familiar.js')}>
+                    Grupo Familiar
+                  </button>
+                </Nav.Link>
+              </li>
+
+              <li className="nav-item">
+                <Nav.Link className="nav-link">
+                  <button
+                    className="btn w-75"
+                    style={{ backgroundColor: '#5A7684', color: '#fff' }}
+                    onClick={VolverALogin}>
+                    Cerrar Sesion
+                  </button>
+                </Nav.Link>
+              </li>
+            </ul>
+          </div>
+        </Nav>
+
+        {/* Contenido principal */}
+        <main className="col-md-10">
+          {children}
+        </main>
+      </Container>
+    </Container>
   );
 }
 
