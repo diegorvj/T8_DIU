f2606b27559c9884cdad2b782903395e26cccf6b
diegorvj <riveradiegoa@gmail.com>
2023-11-16 16:55:44 -0300

    Reestructuracion inicio

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,22 +1,59 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
 import Sidebar from './Sidebar';
+import Nav from 'react-bootstrap/Nav';
+import CardGroup from 'react-bootstrap/CardGroup';
+import g1 from '../imagenes/g1.jpg';
+import g2 from '../imagenes/g2.jpeg';
+import g3 from '../imagenes/g3.jpg';
 
 const Index = () => {
   
   return (
 
     <Sidebar>
+
+<h2 className='h1'style={{ textAlign: 'left', marginLeft: '5%', marginTop: '0%' }}>Sobre nosotros</h2>
       {/* Contenido principal aquí */}
-      <h2 className='h1'style={{ textAlign: 'left', marginLeft: '15%', marginTop: '0%' }}>Somos su clínica de confianza</h2>
-      <p className='text-dark' style={{ textAlign: 'left', marginLeft: '20%', marginTop: '0%' }}>Pioneros en tratamientos</p>
-      <p className='text-dark' style={{ textAlign: 'left', marginLeft: '20%', marginTop: '0%' }}>Contamos con los mejores profesionales del país</p>
-      <p className='text-dark' style={{ textAlign: 'left', marginLeft: '20%', marginTop: '0%' }}>Laboratorios de estándar mundial</p>
-      <p className='text-dark' style={{ textAlign: 'left', marginLeft: '20%', marginTop: '0%' }}>Variedad de procedimientos y especialidades</p>
+
+      <CardGroup>
+
+        <Card bg="dark" text="white" style={{marginLeft: '5%'}}>
+          <Card.Img variant="top" src={g1} style={{ height: '300px' }}/>
+          <Card.Body style={{backgroundColor: '#5A7684'}}>
+            <Card.Title>Somos su clínica de confianza</Card.Title>
+            <Card.Text>
+            Contamos con un equipo de profesionales altamente capacitados que aseguran resultados precisos y confiables en todos los análisis. 
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card bg="dark" text="white">
+          <Card.Img variant="top" src={g2} style={{ height: '300px' }}/>
+          <Card.Body style={{backgroundColor: '#5A7684'}}>
+            <Card.Title>Laboratorios de estándar mundial</Card.Title>
+            <Card.Text>
+            Nuestros laboratorios cuentan con tecnología de vanguardia y cumplen con los más altos estándares a nivel mundial. Estamos comprometidos con la excelencia en cada análisis y prueba diagnóstica que realizamos. 
+
+
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+        <Card bg="dark" text="white" style={{marginRight:'5%'}}>
+          <Card.Img variant="top" src={g3} style={{ height: '300px' }}/>
+          <Card.Body style={{backgroundColor: '#5A7684'}}>
+            <Card.Title>Variedad de procedimientos y especialidades</Card.Title>
+            <Card.Text>
+            En nuestra clínica, ofrecemos una amplia variedad de procedimientos médicos y especialidades para satisfacer todas tus necesidades de salud. Desde consultas generales hasta tratamientos especializados, nuestro equipo de profesionales altamente calificados está aquí para brindarte atención personalizada y de calidad. 
+            </Card.Text>
+          </Card.Body>
+        </Card>
+
+    </CardGroup>
     </Sidebar>
-    
-   
-    
   );
 }
 
